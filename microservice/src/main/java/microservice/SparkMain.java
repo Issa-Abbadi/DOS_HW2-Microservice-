dee9a877ca2dc71bc230fa6a2ee1c3157ca38936
Issa Abbadi <71350660+Issa-Abbadi@users.noreply.github.com>
2022-10-22 08:57:32 +0300

    Added Almost All Functionality in one Microservice
    Added Almost All Functionality in one Microservice. but that's not distributed so we need to divide it into 3 microservices.

--- microservice/src/main/java/microservice/SparkMain.java
+++ microservice/src/main/java/microservice/SparkMain.java
@@ -9,19 +9,26 @@ import java.util.List;
 
 import com.google.gson.Gson;
 
+import spark.Response;
+
 import static spark.Spark.port;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;  
 import java.sql.DriverManager;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SparkMain   {
 	
 	
 	 private  Connection connect() {  
 	        // SQLite connection string  
-		 String url = "jdbc:sqlite:C:/sqlite/gui/SQLiteStudio/microservicedb";  
+		 String url = "jdbc:sqlite:microservicedb";  
 	        Connection conn = null;  
 	        try {  
 	            conn = DriverManager.getConnection(url); 
@@ -32,80 +39,125 @@ public class SparkMain   {
 	        return conn;  
 	    }  
 	 
-	 public void insert() {  
-		 String sql = "INSERT INTO products(id) VALUES(450)";   
+  
+	 public ResultSet search(String topic) {  
+		 String sql = "SELECT id,title FROM  products WHERE topic = ?";   
 	   
 	        try{  
 	            Connection conn = this.connect();  
 	            PreparedStatement pstmt = conn.prepareStatement(sql);  
-//	            pstmt.setString(1, name);  
-//	            pstmt.setDouble(2, capacity);  
-	            pstmt.executeUpdate();  
+	            pstmt.setString(1, topic);   
+	            ResultSet res =  pstmt.executeQuery();
+	            return res;
 	        } catch (SQLException e) {  
 	            System.out.println(e.getMessage());  
-	        }  
-	    }  
+	        }
+	        System.out.println("Not Found");
+			return null;  
+	    } 
+	 
+	 public ResultSet info(int id) {  
+		 String sql = "SELECT title,price,quantity FROM  products WHERE id = ?";   
+	   
+	        try{  
+	            Connection conn = this.connect();  
+	            PreparedStatement pstmt = conn.prepareStatement(sql);  
+	            pstmt.setInt(1, id);  
+	            ResultSet res =  pstmt.executeQuery();
+	            return res;
+	        } catch (SQLException e) {  
+	        
+	            System.out.println(e.getMessage());  
+	        }
+	    	System.out.println("Not Found");
+			return null;  
+	    } 
+	 
+	 
+	 
+	 public ResultSet purchase(int id) throws NumberFormatException, JSONException, Exception {  
+		 String sql = "SELECT quantity FROM  products WHERE id = ?";   
+		 
+	   
+	        try{  
+	            Connection conn = this.connect();  
+	            PreparedStatement pstmt = conn.prepareStatement(sql);  
+	            pstmt.setInt(1, id);  
+	            ResultSet res =  pstmt.executeQuery();
+	          
+	            if(res.getInt(1) > 0) {
+	            
+	            	 String sql2 = "UPDATE products SET quantity = ? WHERE id = ?"; 
+	            	 PreparedStatement pstmt2 = conn.prepareStatement(sql2);  
+	            	 pstmt2.setInt(1,  res.getInt(1)-1);
+	 	            pstmt2.setInt(2, id);  
+	 	              pstmt2.executeUpdate();
+	 	             res =  pstmt.executeQuery();
+	 	            return  res;
+	            }else  if(!res.next() ) {
+	            	System.out.println("Not Found");
+	            }else{
+	            	System.out.println("Sold Out");
+	            }
+	         
+	            return res;
+	        } catch (SQLException e) {  
+	        	
+	            System.out.println(e.getMessage());  
+	        }
+	        System.out.print("Not Found");
+			return null;  
+	    } 
+	 
+	 
+	 
+	 public static JSONArray convert(ResultSet resultSet) throws Exception {
+		 
+		    JSONArray jsonArray = new JSONArray();
+		 
+		    while (resultSet.next()) {
+		 
+		        int columns = resultSet.getMetaData().getColumnCount();
+		        JSONObject obj = new JSONObject();
+		 
+		        for (int i = 0; i < columns; i++)
+		            obj.put(resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase(), resultSet.getObject(i + 1));
+		 
+		        jsonArray.put(obj);
+		    }
+		    return jsonArray;
+		}
 	public static void main(String[] args) {
 		
 		 SparkMain app = new SparkMain();
-		 app.insert();
-	
 		
-//		int x = 500;
-//		
-//		
-//		 try {
-//			 Connection conn = this.connect();  
-//			PreparedStatement pstmt = conn.prepareStatement(sql);  
-////			 pstmt.setInt(1, x);
-//			 pstmt.executeUpdate();  
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}  
-//		
-//		sparkMethods methods = new sparkMethods();
-//		 
-//		 //configure port
-//		 port(8087);
-//		 
-//		 get("/hello", (req, res) -> "Hello, world!");
-//		 
-//		 get("/hello/:name", (req,res)->{
-//		            return "Hello, "+ req.params(":name") + "!!";
-//		        });
-//		 post("/users/add", (req,res)-> {
-//			 
-//			 res.type("application/json");
-//			 
-//			 User user = new Gson().fromJson(req.body(), User.class);
-//			 methods.addUser(user);
-//			 
-//			 return new Gson().toJson(user);
-//			});
-//			get("/users", (req,res)-> {
-//				 res.type("application/json");
-//				 return new Gson().toJson(methods.getUsers());
-//				});
-//			
-//			put("/users/edit/:id", (req,res) -> {
-//				 res.type("application/json");
-//				 User ue = new Gson().fromJson(req.body(), User.class);
-//				 
-//				 User searchedUser = methods.getUserById(ue.getId());
-//				 if(searchedUser != null) {
-//				 methods.editUser(ue);
-//				 return new Gson().toJson(ue);
-//				 } else {
-//				 return new Gson().toJson("User not found or error in edit");
-//				 }
-//				 
-//				});
-//			delete("/users/delete/:id", (req,res) -> {
-//				 res.type("application/json");
-//				methods.deleteUserById(req.params(":id"));
-//				 return new Gson().toJson("User deleted");
-//				});
+		 
+		 
+
+
+		 port(8089);
+		 
+		 get("/search/:topic", (req, res) -> {
+			 res.type("application/json");
+			   
+			 return app.convert(app.search(req.params(":topic")));
+		 });
+		 
+		 get("/info/:id", (req, res) -> {
+			 res.type("application/json");
+			   
+			 return app.convert(app.info(Integer.parseInt(req.params(":id"))));
+		 });
+		 
+		 
+		 put("/purchase/:id", (req,res) -> {
+			
+			 res.type("application/json");
+			 
+			 return app.convert(app.purchase(Integer.parseInt(req.params(":id"))));
+		 });
+
+
 
 		 }
 
